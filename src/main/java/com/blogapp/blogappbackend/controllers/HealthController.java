367edf64ce51c1d079ae85419257a00617461de5
Akash <akshalder11@gmail.com>
2025-10-20 12:18:23 +0530

    BlogAppBackendApplication - v1.0.1 #HealthController - Returning map instead of String

--- src/main/java/com/blogapp/blogappbackend/controllers/HealthController.java
+++ src/main/java/com/blogapp/blogappbackend/controllers/HealthController.java
@@ -1,13 +1,16 @@
 package com.blogapp.blogappbackend.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/api/health")
-    public String healthCheck () {
-        return "Backend is up and running";
+    public ResponseEntity<?> healthCheck () {
+        return ResponseEntity.ok().body(Map.of("status", "Backend is up and running"));
     }
 }
